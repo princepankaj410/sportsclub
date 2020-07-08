d9a986bd1cf6ec749510d846625001b7254d0fb0
princepankaj410 <60131787+princepankaj410@users.noreply.github.com>
2020-07-08 11:35:42 +0530

    first commit

---
+++ worldsports.club/wp-content/plugins/sportspress-for-cricket/js/sportspress-cricket8daf.js
@@ -0,0 +1,10 @@
+(function($) {
+	var a = $('.sp-template-event-performance-team-0-section-1')[0];
+	var b = $('.sp-template-event-performance-team-1-section-1')[0];
+	if ( undefined !== a && undefined !== b ) {
+		var t = a.parentNode.insertBefore(document.createTextNode(''), a); 
+		b.parentNode.insertBefore(a, b); 
+		t.parentNode.insertBefore(b, t); 
+		t.parentNode.removeChild(t); 
+	}
+})(jQuery);
